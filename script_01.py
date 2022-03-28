61fd7784b46953526336d4ddfe43b835f6d4534e
adken <29119766+adken@users.noreply.github.com>
2022-03-28 11:19:58 +0200

    Create script_01.py
    Python script created in Spyder

---
+++ script_01.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 11:09:49 2022
+
+@author: Adken
+"""
+# Importing libraries
+import os
+import sys
+
+print(sys.path)
+help("modules")
+
+# Python variable declaration
+days_of_the_week = 7
+
+# Python function say_hello
+def say_hello(recipient):
+    print("Hello, world! Hello{}".format(recipient))
+    return recipient
+
